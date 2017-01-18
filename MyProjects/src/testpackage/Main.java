dd2091943665a8eb87af793b5a7ff47336843e65
PavelYepihin <pashaepihin40@gmail.com>
2017-01-18 20:28:29 +0200

    test

---
+++ MyProjects/src/testpackage/Main.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+/**
+ * Created by Администратор on 18.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
